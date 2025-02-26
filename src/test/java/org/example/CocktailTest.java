0040995b5d6d3e186615788fd348fbdce9d6c093
Samisb2002 <150432679+Samisb2002@users.noreply.github.com>
2025-02-26 11:21:51 +0100

    Update CocktailTest.java

--- src/test/java/org/example/CocktailTest.java
+++ src/test/java/org/example/CocktailTest.java
@@ -1,23 +1,43 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CocktailTest {
 
     @Test
-    void add() {
-        fail();
+    void testCocktailSansAlcool() {
+        Cocktail cocktail = new Cocktail("Mojito");
+        assertEquals("Mojito", cocktail.getNom());
+        assertTrue(cocktail.alcoolFree());
     }
 
     @Test
-    void alcoolFree() {
-        fail();
+    void testAddIngredient() {
+        Cocktail cocktail = new Cocktail("Mojito");
+        cocktail.add("Menthe", 10.0);
+        cocktail.add("Citron", 20.0);
+
+        // Vérifiez que les ingrédients sont correctement ajoutés via toString()
+        String expected = "Menthe 10.0%\tCitron 20.0%\t";
+        assertEquals(expected, cocktail.toString());
     }
 
     @Test
     void testToString() {
-        fail();
+        Cocktail cocktail = new Cocktail("Mojito");
+        cocktail.add("Menthe", 10.0);
+        cocktail.add("Citron", 20.0);
+
+        String expected = "Menthe 10.0%\tCitron 20.0%\t";
+        assertEquals(expected, cocktail.toString());
     }
+
+    @Test
+    void testGetNom() {
+        Cocktail cocktail = new Cocktail("Mojito");
+        assertEquals("Mojito", cocktail.getNom());
+    }
+
+
 }
