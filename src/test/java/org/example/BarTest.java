42009796063ee4f69ab7898797064a5f406a5b82
Samisb2002 <150432679+Samisb2002@users.noreply.github.com>
2025-02-26 11:19:32 +0100

    Update BarTest.java

--- src/test/java/org/example/BarTest.java
+++ src/test/java/org/example/BarTest.java
@@ -1,50 +1,138 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Vector;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BarTest {
-    public Vector<Boisson> boissonChaude;
-    public Vector<Boisson> boissonFroide;
-    public Vector<Boisson> boissonAlcoolisee;
-    public Vector<Cocktail> cocktailSansAlcoole;
-    public Vector<Cocktail> cocktailAvecAlcoole;
 
+    @Test
+    void testAddBoisson() {
+        Bar bar = new Bar();
+        Boisson coca = new Boisson("Coca");
+        Boisson whisky = new Boisson("Whisky", 40.0f);
 
-    @BeforeEach
-    void setUp(){
-        this.boissonChaude = new Vector<Boisson>();
-        this.boissonFroide = new Vector<Boisson>();
-        this.boissonAlcoolisee = new Vector<Boisson>();
-        this.cocktailAvecAlcoole = new Vector<Cocktail>();
-        this.cocktailSansAlcoole = new Vector<Cocktail>();
+        // Ajouter une boisson sans alcool
+        bar.add(coca);
+        assertEquals(1, bar.getBoissonFroide().size());
+        assertTrue(bar.getBoissonFroide().contains(coca));
+
+        // Ajouter une boisson alcoolisée
+        bar.add(whisky);
+        assertEquals(1, bar.getBoissonAlcoolisee().size());
+        assertTrue(bar.getBoissonAlcoolisee().contains(whisky));
     }
 
     @Test
-    void add() {
-        assertNull(this.boissonAlcoolisee);
-        assertNull(this.boissonChaude);
-        assertNull(this.boissonFroide);
-        assertNull(this.cocktailAvecAlcoole);
-        assertNull(this.cocktailSansAlcoole);
+    void testAddCocktail() {
+        Bar bar = new Bar();
+        Cocktail mojito = new Cocktail("Mojito");
+        mojito.add("Menthe", 10.0);
+        mojito.add("Citron", 20.0);
+
+        // Ajouter un cocktail sans alcool
+        bar.add(mojito);
+        assertEquals(1, bar.getCocktailSansAlcool().size());
+        assertTrue(bar.getCocktailSansAlcool().contains(mojito));
     }
 
     @Test
-    void testAdd() {
-        fail();
+    void testServBoisson() {
+        Bar bar = new Bar();
+        Boisson coca = new Boisson("Coca");
+        Boisson whisky = new Boisson("Whisky", 40.0f);
+
+        // Ajouter des boissons
+        bar.add(coca);
+        bar.add(whisky);
+
+        // Servir une boisson sans alcool
+        Boisson servedCoca = (Boisson) bar.serv("Coca");
+        assertNotNull(servedCoca);
+        assertEquals("Coca", servedCoca.getNom());
+        assertEquals(0, bar.getBoissonFroide().size()); // La boisson a été retirée
+
+        // Servir une boisson alcoolisée
+        Boisson servedWhisky = (Boisson) bar.serv("Whisky");
+        assertNotNull(servedWhisky);
+        assertEquals("Whisky", servedWhisky.getNom());
+        assertEquals(0, bar.getBoissonAlcoolisee().size()); // La boisson a été retirée
     }
 
     @Test
-    void serv() {
-        fail();
+    void testServCocktail() {
+        Bar bar = new Bar();
+        Cocktail mojito = new Cocktail("Mojito");
+        mojito.add("Menthe", 10.0);
+        mojito.add("Citron", 20.0);
+
+        // Ajouter un cocktail
+        bar.add(mojito);
+
+        // Servir un cocktail
+        Cocktail servedMojito = (Cocktail) bar.serv("Mojito");
+        assertNotNull(servedMojito);
+        assertEquals("Mojito", servedMojito.getNom());
+        assertEquals(0, bar.getCocktailSansAlcool().size()); // Le cocktail a été retiré
+    }
+
+    @Test
+    void testServNonExistent() {
+        Bar bar = new Bar();
+        Boisson coca = new Boisson("Coca");
+        bar.add(coca);
+
+        // Essayer de servir une boisson qui n'existe pas
+        Object served = bar.serv("Eau");
+        assertNull(served); // Doit retourner null
     }
 
     @Test
     void testToString() {
-        fail();
+        Bar bar = new Bar();
+        Boisson coca = new Boisson("Coca");
+        Boisson whisky = new Boisson("Whisky", 40.0f);
+        Cocktail mojito = new Cocktail("Mojito");
+        mojito.add("Menthe", 10.0);
+        mojito.add("Citron", 20.0);
+
+        // Ajouter des boissons et un cocktail
+        bar.add(coca);
+        bar.add(whisky);
+        bar.add(mojito);
+
+        // Utiliser System.lineSeparator() pour les fins de ligne
+        String eol = System.lineSeparator();
+        String expected = "Bar:" + eol +
+                "\t Sans alcool" + eol +
+                "\t\tCoca" + eol +
+                "\t Avec alcool" + eol +
+                "\t\tWhisky (l'abus d'alcool est dangereux pour la sante)" + eol +
+                "\t Cocktail sans alcool" + eol +
+                "\t\tMenthe 10.0%\tCitron 20.0%\t" + eol +
+                "\t Cocktail avec alcool" + eol +
+                "\t Boissons chaudes" + eol;
+
+        assertEquals(expected, bar.toString());
+    }
+    @Test
+    void testGetLists() {
+        Bar bar = new Bar();
+        Boisson coca = new Boisson("Coca");
+        Boisson whisky = new Boisson("Whisky", 40.0f);
+        Cocktail mojito = new Cocktail("Mojito");
+        mojito.add("Menthe", 10.0);
+        mojito.add("Citron", 20.0);
+
+        // Ajouter des éléments
+        bar.add(coca);
+        bar.add(whisky);
+        bar.add(mojito);
+
+        // Vérifier les listes
+        assertEquals(1, bar.getBoissonFroide().size());
+        assertEquals(1, bar.getBoissonAlcoolisee().size());
+        assertEquals(1, bar.getCocktailSansAlcool().size());
+        assertEquals(0, bar.getCocktailAvecAlcool().size());
+        assertEquals(0, bar.getBoissonChaude().size());
     }
 }
